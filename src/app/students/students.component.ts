bd328cf9b4c2d05e44068ef9477ee96b638f92b5
Peter Gunardi <peter.gunardi@expeditors.com>
2022-12-29 12:08:51 -0800

    End S3 V47: Implementing Pagination on the Table

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Student } from '../models/ui-models/student.model';
 import { StudentService } from './student.service';
@@ -12,6 +13,8 @@ export class StudentsComponent implements OnInit {
   students: Student[] = [];
   displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile', 'gender'];
   dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+
 
   constructor( private studentService: StudentService) { }
 
@@ -22,6 +25,11 @@ export class StudentsComponent implements OnInit {
       (successResponse) => {
         this.students = successResponse;
         this.dataSource = new MatTableDataSource<Student>(this.students);
+
+        if(this.matPaginator) {
+          this.dataSource.paginator = this.matPaginator; //This is where we hook the dataSource to the paginator.
+        }
+
       },
       (errorResponse) => {
         console.log(errorResponse);
